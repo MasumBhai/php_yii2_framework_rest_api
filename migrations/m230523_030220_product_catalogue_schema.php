7cb114da0dba31231d38ee8def4c74b7f7ade8f8
MasumBhai <abdullahmasum6035@gmail.com>
2023-05-23 09:05:46 +0600

    mysql database connected & schema implemented

---
+++ migrations/m230523_030220_product_catalogue_schema.php
@@ -0,0 +1,98 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230523_030220_product_catalogue_schema
+ */
+class m230523_030220_product_catalogue_schema extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%user}}', [
+            'user_id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+            'verification_token' => $this->string()->defaultValue(null),
+        ], $tableOptions);
+
+        $this->createTable('{{%product}}', [
+            'product_id' => $this->primaryKey(),
+
+            'product_name' => $this->string( 256)->notNull(),
+            'product_price' => $this->decimal(10, 2)->notNull(),
+            'product_description' => $this->text(),
+        ], $tableOptions);
+
+        $this->createTable('{{%cart}}', [
+            'cart_id' => $this->primaryKey(),
+
+            'user_id' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->createTable('{{%cart_item}}', [
+            'cart_item_id' => $this->primaryKey(),
+
+            'cart_id' => $this->integer()->notNull(),
+            'product_id' => $this->integer()->notNull(),
+            'product_quantity' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->addForeignKey('fk_cart_customer', '{{%cart}}', 'user_id',
+            '{{%user}}', 'user_id',
+            'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk_cart_item_cart', '{{%cart_item}}', 'cart_id',
+            '{{%cart}}', 'cart_id',
+            'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk_cart_item_product', '{{%cart_item}}', 'product_id',
+            '{{%product}}', 'product_id',
+            'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // But first need to drop foreign key constraints
+        $this->dropForeignKey('fk_cart_item_product', '{{%cart_item}}');
+        $this->dropForeignKey('fk_cart_item_cart', '{{%cart_item}}');
+        $this->dropForeignKey('fk_cart_customer', '{{%cart}}');
+
+        // After that, dropping tables in reverse order
+        $this->dropTable('{{%cart_item}}');
+        $this->dropTable('{{%cart}}');
+        $this->dropTable('{{%product}}');
+        $this->dropTable('{{%user}}');
+    }
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230523_030220_product_catalogue_schema cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
